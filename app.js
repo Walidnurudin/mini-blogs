a3463edafd31635e4e50e5f6cdbb1f3eb5637662
Walid nurudin <walidnurudin47@gmail.com>
2020-09-29 16:12:21 +0700

    add details page

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // middleware & static files
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'))
 
 // route
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
     res.render('home', { title: "home" })
 });
 
+// api
 app.get('/blogs', (req, res) => {
     Blog.find().sort({ createdAt: -1 })
     .then(result => {
@@ -32,6 +34,31 @@ app.get('/blogs', (req, res) => {
     })
     .catch(err => console.log(err))
 });
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+
+    blog.save()
+    .then(result => {
+        res.redirect('/blogs')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+// end of api
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', {blog: result, title: 'Detail page'});
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
 
 app.get('/about', (req, res) => {
     res.render('about', { title: "about" })
