32a765f2a0940c773f85de53b443f9d56ee37edd
Walidnurudin <walidnurudin47@gmail.com>
2020-10-25 22:55:21 +0700

    shitty

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs')
 
 // connect mongodb with dotenv
 require('dotenv').config();
-const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vt15h.mongodb.net/first-basic?authMechanism=MONGODB-CR`;
+const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vt15h.mongodb.net/first-basic?retryWrites=true&w=majority`;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     // jika connect ke db, maka app berjalan
